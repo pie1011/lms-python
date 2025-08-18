ee2003a3f9a04c2fc13647d06ae6a80ef80bbb1b
Katie <pie10101011@gmail.com>
2025-08-18 19:32:52 +0000

    starting student portal

--- lms_platform/urls.py
+++ lms_platform/urls.py
@@ -1,18 +1,5 @@
 """
 URL configuration for lms_platform project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
 from django.urls import path
@@ -26,6 +13,11 @@ from lms_platform.core.admin import admin_site
 urlpatterns = [
     path("", core_views.index, name='index'), 
     path("admin/", admin_site.urls),
+    
+    # Student Portal URLs
+    path("student/", core_views.student_dashboard, name='student_dashboard'),
+    path("student/login/", core_views.student_login, name='student_login'),
+    path("student/logout/", core_views.student_logout, name='student_logout'),
 ]
 
 if settings.DEBUG:
